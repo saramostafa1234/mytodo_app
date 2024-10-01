526ad886336698a93335fb6674f0b4deaa34d672
SARA Mostafa <smsh200355@gmail.com>
2024-10-01 12:50:35 +0300

    Initial Project. * Added empty home.

---
+++ lib/home/homescreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'Home_screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
